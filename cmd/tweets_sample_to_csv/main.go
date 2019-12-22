1d9636bdf3b5e3ff201ab6cb56d68c712a49f415
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2019-12-22 19:50:39 +0000

    Filename from command line.

--- cmd/tweets_sample_to_csv/main.go
+++ cmd/tweets_sample_to_csv/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 
 	"github.com/dimitrismistriotis/tweets_sample_to_csv/internal/tweetssampletocsv"
 )
 
 func main() {
 	fmt.Println("Entry point")
+	targetFilename := flag.String("filename", "",
+		"File to store samples, will exit if already exists or cannot write to location")
+	flag.Parse()
+
+	fmt.Printf("targetFilename: %s\n", *targetFilename)
+	if *targetFilename == "" {
+		log.Fatal("Empty filename provided")
+	}
 	// tweets_sample_to_csv.
 	tweetssampletocsv.RetrieveAndStore()
 	fmt.Println("After entry point")
