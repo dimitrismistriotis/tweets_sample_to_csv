c9514c505f5fefb43358eeafc87013e3decbd08f
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2019-12-26 19:53:35 +0000

    Add optional max sample size.

--- cmd/tweets_sample_to_csv/main.go
+++ cmd/tweets_sample_to_csv/main.go
@@ -11,12 +11,15 @@ import (
 func main() {
 	fmt.Println("Entry point")
 	targetFilename := flag.String("filename", "", "File to store samples")
+	maxItemsToDownload := flag.Int64("items_to_download", -1, "Max sample size, -1 (default) for infinite")
 	flag.Parse()
 
 	fmt.Printf("targetFilename: %s\n", *targetFilename)
 	if *targetFilename == "" {
 		log.Fatal("Empty filename provided")
 	}
+	fmt.Printf("maxItemsToDownload (not used yet): %d\n", *maxItemsToDownload)
+
 	// tweets_sample_to_csv.
 	tweetssampletocsv.RetrieveAndStore()
 	fmt.Println("After entry point")
